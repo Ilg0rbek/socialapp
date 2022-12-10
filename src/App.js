7a70fa5e1e191ee4c00bb0af275f7965ae6e7edc
Ilgorbek Abdumalikov <ilgorbekabdumalikov331@gmail.com>
2022-12-10 14:24:39 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
+import React, { useMemo } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import Navbar from "./scenes/navbar";
 
-function App() {
-  return <div>Hello social media</div>;
-}
+const App = () => {
+  const mode = useSelector((state) => state.getInitialState.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
