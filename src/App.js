162da5753ee798f3844a89cfeb1dd5240e808df3
Ilg0rbek <87908082+Ilg0rbek@users.noreply.github.com>
2023-01-08 16:14:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
 
 function App() {
+  
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   const isAuth = Boolean(useSelector((state) => state.token));
+  
   return (
     <div className="app">
       <BrowserRouter>
@@ -33,4 +35,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
